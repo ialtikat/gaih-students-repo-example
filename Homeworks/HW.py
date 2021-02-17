4f86c762429973453db643f18d20ae46d87909d9
İbrahim Altıkat <65372029+ialtikat@users.noreply.github.com>
2021-02-17 21:55:20 +0300

    Delete Homeworks directory

--- Homeworks/HW.py
+++
@@ -1,7 +0,0 @@
-import random 
-asal=(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97) #asal sayılar listemiz
-liste=[[random.choice(asal) for x in range(3)] for x in range(3)] #Matrisi oluşturma
-for i in range(3):
-    print(liste[i]) #Matrisi yazdırma
-    
-
